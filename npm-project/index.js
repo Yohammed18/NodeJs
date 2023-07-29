509f0ae0dc1a9a542089c17c97c254be2a3b8096
Mohammed Allibalogun <mallibalogun18@gmail.com>
2023-07-29 18:00:54 -0400

    Install and working with external packages.

---
+++ npm-project/index.js
@@ -0,0 +1,8 @@
+//this is how we will be able to get a hold of the module. this is similar to react with the key word Import.
+
+
+// var generateName = require('sillyname');
+import generateName from 'sillyName'
+var sillyName = generateName();
+console.log(`My name is ${sillyName}`)
+
