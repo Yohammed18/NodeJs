cd6d4cd55a3812462584a2748db2a16632bf0701
Mohammed Allibalogun <mallibalogun18@gmail.com>
2023-08-02 08:09:14 -0400

    node basic

---
+++ NodeFullCourse/server.js
@@ -0,0 +1,24 @@
+//Nodes runs on the server (strictly focus on handling back-end work and development.)
+
+//console is the termonal window
+console.log("Welcome to the Full Node JS Tutorial.")
+//Node uses the object global instead of the window object that we use in the web browser .
+console.log(global)
+//The common core module that NodeJs provides are OS, Path, and direct
+//CommonJS uses a require() statement, ES6 uses the imports statement but they basically do the same thing
+const os = require('os')
+const path = require('path')
+console.log(os.type())
+console.log(os.version())
+console.log(os.homedir())
+
+//to get the directory or file name use the global veriables below
+console.log(`Directory: ${__dirname}`)
+console.log(`File: ${__filename}`)
+console.log(path.dirname(__filename))
+console.log(`Name of the file: ${path.basename(__filename)}`)
+console.log(`File ext: ${path.extname(__filename)}`)
+
+console.log(path.parse(__filename))
+
+
